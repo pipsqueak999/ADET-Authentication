7c4b88f24b1eebd9c69eb4e63257984328ffcfd6
Francis Icaro <francissinogbaicaro@gmail.com>
2025-11-26 13:54:33 +0800

    register page modify

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -10,11 +10,22 @@ import { API_BASE } from '@/lib/config';
 
 import { FormEvent } from 'react';
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 export default function RegisterPage() {
 
   const router = useRouter();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [sex, setSex] = useState('');
+  const [age, setAge] = useState('');
   const [error, setError] = useState('');
 
   async function handleRegister(e: FormEvent) {
@@ -38,10 +49,21 @@ export default function RegisterPage() {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <Card className="w-full max-w-sm p-6">
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover z-0 brightness-50">
+        <source src="bg-vid.mp4" type="video/mp4" />
+      </video>
+      <div className="absolute inset-0 bg-black/40 z-10"></div>
 
+      <Card className="w-full max-w-lg p-6 z-30 backdrop-blur-xl bg-white/10 ">
         <CardContent>
-          <h1 className="text-xl font-bold mb-4">Register</h1>
+          <h1 className="flex text-3xl font-bold text-[#1322A3] mb-10 justify-center">
+            Create New Account
+          </h1>
 
           <form onSubmit={handleRegister} className="space-y-4">
             <Input
@@ -51,11 +73,43 @@ export default function RegisterPage() {
             />
 
             <Input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
               type="password"
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
+
+            <div className="flex gap-4">
+              <Select>
+                <SelectTrigger className="w-[50%]">
+                  <SelectValue placeholder="Sex" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="male">Male</SelectItem>
+                  <SelectItem value="female">Female</SelectItem>
+                  <SelectItem value="non-binary">Non-binary</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <Select>
+                <SelectTrigger className="w-[50%]">
+                  <SelectValue placeholder="Age" />
+                </SelectTrigger>
+                <SelectContent>
+                  {Array.from({ length: 88 }, (_, i) => (
+                    <SelectItem key={i} value={`option ${i + 1}`}>
+                      {i + 13}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
 
             {error && <p className="text-red-500 text-sm">{error}</p>}
 
